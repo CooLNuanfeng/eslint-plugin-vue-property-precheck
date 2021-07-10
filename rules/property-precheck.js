b1755aa88a7a44bfa14c342243e83e37d52fae5f
wuhongsong <wuhongsong@haier.com>
2021-07-10 14:47:54 +0800

    init

---
+++ rules/property-precheck.js
@@ -0,0 +1,29 @@
+module.exports = {
+  meta: {
+      docs: {
+        description: "no console.time()",
+        category: "Fill me in",
+        recommended: false
+      },
+      fixable: null,  // or "code" or "whitespace"
+      schema: [
+        // fill in your schema
+      ],
+      // 报错信息描述
+      messages: {
+        avoidMethod: "console method '{{name}}' is forbidden.",
+      },
+  },
+
+  create: function(context) {
+    return context.parserServices.defineTemplateBodyVisitor(
+      // Event handlers for <script> or scripts. (optional)
+      {
+        MemberExpression(node) {
+
+          console.log(node)
+        }
+      }
+    )
+  }
+};
---
+++ test/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+const arr = []
+export default {
+  mounted () {
+    this.name = arr[0].name
+  }
+}
+</script>
+
+<style>
+
+</style>
